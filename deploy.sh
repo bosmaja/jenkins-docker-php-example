1d7b688cd088996596048f89e2f9eecb6a7024c7
Jan Bosmans <jan.bosmans@gmail.com>
2017-03-27 17:15:05 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker kill simple-web > /dev/null 2>&1
 docker rm simple-web > /dev/null 2>&1
-docker rmi simple-web:latest > /dev/null 2>&1
 docker run -d -p 80:80 --name simple-web simple-web
